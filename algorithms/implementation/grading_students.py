9378ae786359eb89c2c58d3244311b22ad7860f1
Luiz Ferreira <lgustavo.fonseca@gmail.com>
2017-11-17 20:06:46 -0200

    Algorithms > Implementation > Grading Students

---
+++ algorithms/implementation/grading_students.py
@@ -0,0 +1,29 @@
+import math
+
+
+def is_grade_enough(grade):
+    if rounded(grade) < 40:
+        return(False)
+    return(True)
+
+
+def rounded(grade):
+    if next_multiple_of_five(grade) - grade < 3:
+        return(next_multiple_of_five(grade))
+    return(grade)
+
+
+def next_multiple_of_five(grade):
+    if grade % 5 == 0:
+        return(grade)
+    next_multiple = math.ceil(grade/5)*5
+    return(next_multiple)
+
+
+students_number = int(input().strip())
+
+for i in range(students_number):
+    grade = int(input().strip())
+    if is_grade_enough(grade):
+        grade = rounded(grade)
+    print(grade)
